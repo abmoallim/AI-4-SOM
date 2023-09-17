208a740dfc9972596f928d3c4ee0add65a52a4da
abmoallim <ahmmoallim@gmail.com>
2023-09-17 12:29:19 +0300

    adding toaster and loading

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ $(document).ready(function () {
   $("#contributionForm").submit(function (e) {
     e.preventDefault();
 
+    // Hide the form and show the loading indicator
+    $("#contributionForm").hide();
+    $("#loadingIndicator").show();
+
+    let _author = "unknown";
+    if ($("#name").val()) {
+      _author = $("#name").val();
+    }
+
     var formData = {
       question: $("#question").val(),
       answer: $("#answer").val(),
       asked: false,
-      author: $("#name").val(),
+      author: _author,
     };
 
     var apiUrl = "http://localhost:30007/api/data";
@@ -21,16 +30,33 @@ $(document).ready(function () {
         "x-api-key": apiKey,
       },
       success: function (response) {
-        $("#contributionForm").hide();
+        // Hide the loading indicator and display a success message
+        $("#loadingIndicator").hide();
         $("#thankYouMessage").show();
 
         toastr.success("Your contribution was submitted successfully!");
       },
       error: function (error) {
+        // Hide the loading indicator and display an error message
+        $("#loadingIndicator").hide();
+        $("#contributionForm").show();
+
         console.error("Error:", error);
 
         toastr.error("An error occurred while submitting your contribution.");
       },
     });
+  });
+
+  $("#addNewButton").click(function () {
+    // Clear the form fields
+    $("#question").val("");
+    $("#answer").val("");
+    $("#name").val("");
+
+    // Show the form and hide the "Thank you" message and "Add New" button
+    $("#contributionForm").show();
+    $("#thankYouMessage").hide();
+    $("#addNewButton").hide();
   });
 });
